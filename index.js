e36cbb01091112d14970f8a9b875c06b959898bb
Josh Dicker <joshdicker@joshs-mac-mini.mynetworksettings.com>
2025-03-19 13:20:39 -0400

    compelted Event Listeners in JavaScript lab

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 // Handle Button Clicks
 
 // Function to change the background color when a button is clicked
+const body = document.querySelector('body')
 function changeBackgroundColor() {
-  // Implement the function to change background color
+  body.style = 'background-color: red'
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
-  // Implement the function to reset background color
+  body.style = 'background-color: white'
 }
 
 // Capture Keyboard Input
 
 // Function to display the key pressed by the user
+const keyPressDisplay = document.getElementById('keyPressDisplay')
+
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  keyPressDisplay.innerText = event.key
+
 }
 
 // Process Text Input
-
+const textInputDisplay = document.getElementById('textInputDisplay')
 // Function to display user input in real-time
-function displayUserInput() {
+function displayUserInput(e) {
   // Implement the function to display user input
+  const { value } = e.target
+  textInputDisplay.innerText = value
 }
+
+function refreshPageToDefault() {
+  keyPressDisplay.innerText = 'Press any key...'
+  textInputDisplay.innerText = 'Your input will be displayed here.'
+  resetBackgroundColor()
+  textInput.value = ""
+}
+
+
+
 
 // Attach Event Listeners
 
@@ -41,3 +58,16 @@ document.addEventListener('keydown', displayKeyPress)
 
 // Attach event listener to display user input in real-time as they type
 document.getElementById('textInput').addEventListener('input', displayUserInput)
+
+// Use mouseover and mouseout events to change element styles when hovering over them:
+keyPressDisplay.addEventListener('mouseover', () => {
+  keyPressDisplay.style.backgroundColor = 'yellow';
+});
+
+//Use mouseover and mouseout events to change element styles when hovering off of them:
+keyPressDisplay.addEventListener('mouseout', () => {
+  keyPressDisplay.style.backgroundColor = 'initial';
+});
+
+//refresh button refresh page values to default
+document.getElementById('refreshForm').addEventListener('click', refreshPageToDefault)
